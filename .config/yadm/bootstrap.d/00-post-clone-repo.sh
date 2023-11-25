ec256f0e3a32b76af010e78af95695d78c65a7fc
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2023-11-25 12:38:24 +0800

    Update post-clone repo bootstrap script for git remotes stuff
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>

--- .config/yadm/bootstrap.d/00-post-clone-repo.sh
+++ .config/yadm/bootstrap.d/00-post-clone-repo.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-echo "Symlinking ~/.git to ~/.local/share/yadm/repo.git for compatibility"
-ln -s ~/.local/share/yadm/repo.git ~/.git
+if [ ! -d "$HOME/.git" ]; then
+    echo "Symlinking ~/.git to ~/.local/share/yadm/repo.git for compatibility"
+    ln -s ~/.local/share/yadm/repo.git ~/.git
+fi
+
+git remote set-url origin ssh://git@mau.dev/andreijiroh.dev/dotfiles
+git remote add hut ssh://git@git.sr.ht/~ajhalili2006/dotfiles
