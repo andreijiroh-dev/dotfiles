d7a95fa8807b109ef1ad97e31aa957272d700ccc
Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>
2024-08-13 23:54:16 +0800

    chore(global): apply patches from termux branch
    See-Also: 00aa74c8255465f7a785dcc0b0f32f44bc0b2886
    See-Also: cbeb6b6b8589f9e3bca51b838a8fbf919aca2a8e
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>

--- .config/yadm/bootstrap.d/00-post-clone-repo.sh
+++ .config/yadm/bootstrap.d/00-post-clone-repo.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-set -x
-
-if [ ! -d "$HOME/.git" ] && [ -d "$HOME/.local/share/yadm/repo.git" ]; then
-    echo "Symlinking ~/.git to ~/.local/share/yadm/repo.git for compatibility"
-    ln -s ~/.local/share/yadm/repo.git ~/.git
-elif [ -d "$HOME/.git" ] && [ ! -d "$HOME/.local/share/yadm/repo.git" ]; then
-    echo "Symlinking ~/.local/share/yadm/repo.git  to ~/.git for compatibility"
-    ln -s ~/.git ~/.local/share/yadm/repo.git
+if [ $DEBUG != "" ]; then
+  set -x
 fi
 
-echo "Setting git remote URLs to SSH"
-git remote set-url --add origin ssh://git@mau.dev/andreijiroh.dev/dotfiles || true
-git remote set-url --add hut ssh://git@git.sr.ht/~ajhalili2006/dotfiles || git remote add hut ssh://git@git.sr.ht/~ajhalili2006/dotfiles
+if [ ! -d "$HOME/.git" ] && [ -d "$HOME/.local/share/yadm/repo.git" ]; then
+    echo "[git-symlinks] Symlinking ~/.git to ~/.local/share/yadm/repo.git for compatibility"
+    ln -s ~/.local/share/yadm/repo.git ~/.git
+elif [ -d "$HOME/.git" ] && [ ! -d "$HOME/.local/share/yadm/repo.git" ]; then
+    echo "[git-symlinks] Symlinking ~/.local/share/yadm/repo.git  to ~/.git for compatibility"
+    ln -s ~/.git ~/.local/share/yadm/repo.git
+else
+    echo "[git-symlinks] Looks like everything is all clear on git"
+fi
+
+echo "[git-remotes] Setting git remote URLs to SSH"
+git remote set-url --add --push hut ssh://git@git.sr.ht/~ajhalili2006/dotfiles \
+  || git remote add hut https://git.sr.ht/~ajhalili2006/dotfiles \
+  && git remote set-url --add --push hut ssh://git@git.sr.ht/~ajhalili2006/dotfiles
